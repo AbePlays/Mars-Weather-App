50d420c3a8614ec1ffb4ef7764ccd284231db5c2
Abe <abhi.rawat456@gmail.com>
2020-03-25 11:06:25 +0530

    Getting data back from NASA

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
 const API_KEY = "DEMO_KEY";
 
-const API_URL = `https: //api.nasa.gov/insight_weather/?api_key=${DEMO_KEY}&feedtype=json&ver=1.0`;
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
+
+function getWeather() {
+  return fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+      const { sol_keys, validity_checks, ...solData } = data;
+      return Object.entries(solData).map(([sol, data]) => {
+        return {
+          sol: sol,
+          maxTemp: data.AT.mx,
+          minTemo: data.AT.mn,
+          windSpeed: data.HWS.av,
+          windDirectionDegrees: data.WD.most_common.compass_degrees,
+          windDirectionCardinal: data.WD.most_common.compass_point,
+          date: new Date(data.First_UTC)
+        };
+      });
+    });
+}
+
+getWeather();
