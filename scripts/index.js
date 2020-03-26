9b34725963b60ad7cbc19509bf2abac80fb228b5
Abe <abhi.rawat456@gmail.com>
2020-03-26 09:28:19 +0530

    Basic styling added

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
+const previousWeatherToggle = document.querySelector(".show-previous-weather");
+const previousWeather = document.querySelector(".previous-weather");
+previousWeatherToggle.addEventListener("click", () => {
+  previousWeather.classList.toggle("show-weather");
+});
+
 const API_KEY = "DEMO_KEY";
 
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
